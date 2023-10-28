e3987947153fdc1113b4257c85a9f69ffc0cabff
kin018 <60412598+kin018@users.noreply.github.com>
2023-10-28 13:34:36 -0400

    Deleted Tim Wilson Files

--- kin018.dev/src/timWilsonThemeCode/godRaysJS.js
+++
@@ -1,8 +0,0 @@
-//Everthing commented out is his code
-
-// function toggleTheme() {
-//     if (document.body.classList.contains("dark"))
-//         document.body.classList.remove("dark");
-//     else
-//         document.body.classList.add("dark");
-// }
