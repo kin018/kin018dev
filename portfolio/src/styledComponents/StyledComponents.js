29396578ff340a0b1727f14f946fbe6db8df3455
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-21 02:43:51 -0500

    updated styles specifically card_light and modifying theme icons

--- portfolio/src/styledComponents/StyledComponents.js
+++ portfolio/src/styledComponents/StyledComponents.js
@@ -8,7 +8,8 @@ export const darkTheme = {
   text_primary: '#F2F3F4',
   text_secondary: '#b1b2b3',
   card: '#171721',
-  card_light: '#191924',
+  card_light: 'rgba(25, 25, 36,.985)', //.985 is alpha channel in other others the transparency, with 0 being completely transparent and 1 being completely opaque
+  // card_light: '#191924', og
   button: '#854CE6',
   white: '#FFFFFF',
   black: '#000000',
@@ -21,7 +22,10 @@ export const lightTheme = {
   text_primary: '#111111',
   text_secondary: '#48494a',
   card: '#FFFFFF',
+  card_light: 'rgba(255, 255, 255, .985)',
   button: '#5c5b5b',
+  white: '#FFFFFF',
+  black: '#000000',
 };
 // App.js main styling
 export const MainAppContainer = styled.div`
@@ -122,10 +126,8 @@ export const StyledNavLink = styled.a`
 
 export const StyledThemeDarkLightLogo = styled.a`
   color: ${({ theme }) => theme.text_primary};
-  font-weight: 500;
   cursor: pointer;
   transition: all 0.2s ease-in-out;
-  text-decoration: none;
 
   :hover {
     color: ${({ theme }) => theme.primary};
