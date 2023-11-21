5ed1c6d5b747c7bb35cadd4fb2a9f72a227bbb39
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-21 13:48:24 -0500

    Updated routing

--- portfolio/src/components/Logo.jsx
+++ portfolio/src/components/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { LogoWrapper } from '../styledComponents/StyledComponents';
 
 function Logo() {
   return (
-    <>
+    <Link to="/">
       <LogoWrapper src="../images/LogoBrackets.png" alt="Logo" />
-    </>
+    </Link>
   );
 }
 
