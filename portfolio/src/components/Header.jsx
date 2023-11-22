b08ead1ca7efbc0ce9af6accf14bf651189833b0
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-22 14:12:26 -0500

    Developed the ThemeIcon Component, seamlessly integrating it into both the Navbar and HeaderComponent. Implemented enhanced styling for icons specifically tailored for MobileView.

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import Navbar from './Navbar';
 import Logo from './Logo';
+import ThemeIcon from './ThemeIcon';
 import {
   AppHeader,
   HeaderMainContainer,
   HeaderBurger,
+  HeaderBurgerNavBtns,
 } from '../styledComponents/StyledComponents';
 
 function Header({ toggleTheme, isDarkTheme }) {
-  // const [theme, setTheme] = useState(darkTheme);
-
   return (
     <AppHeader>
       <HeaderMainContainer>
         <Logo />
         <HeaderBurger>
-          <FaBars />
+          <HeaderBurgerNavBtns>
+            <ThemeIcon toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
+            <FaBars />
+          </HeaderBurgerNavBtns>
         </HeaderBurger>
         <Navbar toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} />
       </HeaderMainContainer>
