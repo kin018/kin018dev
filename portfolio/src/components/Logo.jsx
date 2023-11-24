606827e73b087d2ac3693bb21a975dc5d6042093
kin018 <60412598+kin018@users.noreply.github.com>
2023-11-24 18:15:53 -0500

    Toggle Logo

--- portfolio/src/components/Logo.jsx
+++ portfolio/src/components/Logo.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { LogoWrapper } from '../styledComponents/StyledComponents';
 import { Link } from 'react-router-dom';
 
-function Logo() {
+function Logo({ isDarkTheme }) {
+  const logoSrc = isDarkTheme
+    ? '../images/LogoWhiteBrackets.png'
+    : '../images/LogoBlackBrackets.png';
+
+  const altText = isDarkTheme ? 'LogoWhite' : 'LogoBlack';
+
   return (
     <Link to="/">
-      <LogoWrapper src="../images/LogoWhiteBrackets.png" alt="Logo" />
+      <LogoWrapper src={logoSrc} alt={altText} />
     </Link>
   );
 }
