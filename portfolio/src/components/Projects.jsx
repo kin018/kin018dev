6e6b384012091028782ba08ec2288431c630d3f3
Kenechukuwu Nwankwo <60412598+kin018@users.noreply.github.com>
2024-04-13 10:41:34 -0400

    Update Projects.jsx temporarily

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,83 +1,83 @@
-import React from 'react';
-import {
-  ProjectMainContainer,
-  ProjectMainTitle,
-  ProjectCarousel,
-  ProjectCarouselSlide,
-  ProjectCarouselSlideImg,
-  ProjectCarouselSlideContainer,
-} from '../styledComponents/StyledComponents';
+// import React from 'react';
+// import {
+//   ProjectMainContainer,
+//   ProjectMainTitle,
+//   ProjectCarousel,
+//   ProjectCarouselSlide,
+//   ProjectCarouselSlideImg,
+//   ProjectCarouselSlideContainer,
+// } from '../styledComponents/StyledComponents';
 
-function Projects() {
-  return (
-    <ProjectMainContainer id="projects">
-      <ProjectMainTitle>Projects</ProjectMainTitle>
-      <ProjectCarousel>
-        <ProjectCarouselSlideContainer>
-          <ProjectCarouselSlide>
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon1"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon2"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon3"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon4"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon5"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon6"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon7"
-            />
-          </ProjectCarouselSlide>
-          <ProjectCarouselSlide>
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon1"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon2"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon3"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon4"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon5"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon6"
-            />
-            <ProjectCarouselSlideImg
-              src="../images/screely.png"
-              alt="comingSoon7"
-            />
-          </ProjectCarouselSlide>
-        </ProjectCarouselSlideContainer>
-      </ProjectCarousel>
-    </ProjectMainContainer>
-  );
-}
+// function Projects() {
+//   return (
+//     <ProjectMainContainer id="projects">
+//       <ProjectMainTitle>Projects</ProjectMainTitle>
+//       <ProjectCarousel>
+//         <ProjectCarouselSlideContainer>
+//           <ProjectCarouselSlide>
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon1"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon2"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon3"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon4"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon5"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon6"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon7"
+//             />
+//           </ProjectCarouselSlide>
+//           <ProjectCarouselSlide>
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon1"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon2"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon3"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon4"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon5"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon6"
+//             />
+//             <ProjectCarouselSlideImg
+//               src="../images/screely.png"
+//               alt="comingSoon7"
+//             />
+//           </ProjectCarouselSlide>
+//         </ProjectCarouselSlideContainer>
+//       </ProjectCarousel>
+//     </ProjectMainContainer>
+//   );
+// }
 
-export default Projects;
+// export default Projects;
